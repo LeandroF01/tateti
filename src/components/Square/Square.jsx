ba8103b0b9a14cac7efc4df8e12a69d394190503
LeandroF01 <leaylio3327@gmail.com>
2023-03-09 13:22:27 -0300

    Add components square

---
+++ src/components/Square/Square.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
